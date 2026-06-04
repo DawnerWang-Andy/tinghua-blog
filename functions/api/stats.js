d44db8348572a4f0dbb3cd8599ba1343fe2c39d4
Dawner <dawnerwang@icloud.com>
2026-06-04 17:32:26 +0800

    修复: total 改为从每日数据实时求和，消除 total:all 计数器不同步问题

--- functions/api/stats.js
+++ functions/api/stats.js
@@ -6,19 +6,16 @@ export async function onRequestGet({ env }) {
   for (const key of list.keys) {
     const val = await env.COUNTERS.get(key.name);
     const num = parseInt(val) || 0;
-    if (key.name === "total:all") {
-      data.total = num;
-    } else if (key.name.startsWith("202")) {
+    if (key.name.startsWith("202")) {
       const [date, ...pathParts] = key.name.split(":");
       const page = pathParts.join(":") || "/";
-      if (!data.pages[page]) data.pages[page] = 0;
-      data.pages[page] += num;
+      data.pages[page] = (data.pages[page] || 0) + num;
       if (date === today) {
         data.pages_today[page] = (data.pages_today[page] || 0) + num;
         data.today += num;
       }
-      if (!data.days[date]) data.days[date] = 0;
-      data.days[date] += num;
+      data.days[date] = (data.days[date] || 0) + num;
+      data.total += num;
     }
   }
 
